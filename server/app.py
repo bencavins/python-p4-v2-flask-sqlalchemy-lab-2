cf1f43b0d02f4a9c7d3acbfd6f6480a683097f30
Ben Cavins <ben.cavins@flatironschool.com>
2023-10-25 16:39:30 -0400

    do lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Customer, Review, Item
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -15,6 +15,16 @@ db.init_app(app)
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
+
+@app.route('/customers/<int:id>')
+def customer_by_id(id):
+    customer = Customer.query.filter(Customer.id == id).first()
+
+    if customer is None:
+        return {'message': 'customer does not exits'}, 404
+    
+    # return customer.to_dict(rules=('-reviews',)), 200
+    return customer.to_dict(), 200
 
 
 if __name__ == '__main__':
